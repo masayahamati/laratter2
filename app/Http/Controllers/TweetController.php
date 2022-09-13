c12e68b816f0db849b9f015e2429702fd3b7f6f7
masaya <masaya.reon@gmail.com>
2022-09-13 20:27:22 +0900

    ログイン機能とマイページ

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use App\Models\Tweet;
+use Auth;
 class TweetController extends Controller
 {
     /**
@@ -36,26 +37,28 @@ class TweetController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        // バリデーション
-  $validator = Validator::make($request->all(), [
-    'tweet' => 'required | max:191',
-    'description' => 'required',
-  ]);
-  // バリデーション:エラー
-  if ($validator->fails()) {
-    return redirect()
-      ->route('tweet.create')
-      ->withInput()
-      ->withErrors($validator);
-  }
-  // create()は最初から用意されている関数
-  // 戻り値は挿入されたレコードの情報
-  $result = Tweet::create($request->all());
-  // ルーティング「todo.index」にリクエスト送信（一覧ページに移動）
-  return redirect()->route('tweet.index');
+   public function store(Request $request)
+  {
+    // バリデーション
+    $validator = Validator::make($request->all(), [
+      'tweet' => 'required | max:191',
+      'description' => 'required',
+    ]);
+    // バリデーション:エラー
+    if ($validator->fails()) {
+      return redirect()
+        ->route('tweet.create')
+        ->withInput()
+        ->withErrors($validator);
     }
+
+    // 🔽 編集 フォームから送信されてきたデータとユーザIDをマージし，DBにinsertする
+    $data = $request->merge(['user_id' => Auth::user()->id])->all();
+    $result = Tweet::create($data);
+
+    // tweet.index」にリクエスト送信（一覧ページに移動）
+    return redirect()->route('tweet.index');
+  }
 
     /**
      * Display the specified resource.
@@ -118,4 +121,15 @@ class TweetController extends Controller
         $result = Tweet::find($id)->delete();
         return redirect()->route('tweet.index');
     }
+
+      public function mydata()
+  {
+    // Userモデルに定義したリレーションを使用してデータを取得する．
+    $tweets = User::query()
+      ->find(Auth::user()->id)
+      ->userTweets()
+      ->orderBy('created_at','desc')
+      ->get();
+    return view('tweet.index', compact('tweets'));
+  }
 }
